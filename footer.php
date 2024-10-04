7de584f86e87cf60bbe1ba0167267b0c406cb467
cyrusgabilan1 <cyrusgabilan1@gmail.com>
2024-10-04 20:34:03 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,4 @@
+<footer>
+    <p>&copy; 2024 Group 6. All rights reserved.</p>
+    <p><a href="privacy-policy.html" class="footer-link">Privacy Policy</a> | <a href="terms-of-service.html" class="footer-link">Terms of Service</a></p>
+</footer>
